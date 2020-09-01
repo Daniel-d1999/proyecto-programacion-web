a24e9e05119a0e9d14fbe7e1947c0c0472ff3e52
daniel <ortizroman123456789@gmail.com>
2020-09-01 15:57:44 -0500

    esto es el index

---
+++ index.php
@@ -0,0 +1,92 @@
+<header>
+    <div class="contenedor">
+        <h1 class="icon-dog">hostalito</h1>
+        <input type="checkbox" id="menu-bar">
+        <label class="fontawesome-align-justify" for="menu-bar"></label>
+           <link rel="stylesheet" href="main.css">
+                <nav class="menu">
+                    <a href="somos.html" accesskey="daniel">nosotros</a>
+                    
+                    <a href="galeria.html">ayuda</a>
+                    <a href="inicio.php">iniciar sesion</a>
+                    <a href="registro.php">crear cuenta</a>
+                    
+                </nav>
+    
+                
+
+            </div>
+    
+                 
+</header>
+        
+        <main>
+            <section id="banner">
+            
+                 
+                  <img src="img/habitaciones1.jpg">
+
+
+                <div class="contenedor">lion militar</h2>
+                    
+    
+            </section>
+            
+            <section id="bienvenidos">
+            <div class="contenedor">
+                <h2>BIENVENIDOS A NUESTRA PAGINA  OFICIAL  EL HOSTALITO</h2>
+                <p>  somos una  de los hostales que brindamos servicio de acuerdo asu nesecidad en la provincia de chincheros  </p>
+                </div>
+            </section>
+            
+            <section id="img/blog">
+                <p></p>
+                <div class="contenedor">
+                    <article>
+                       <p></p>
+                            <p>son amobladas para el cliente</p>
+                        <p></p>
+                        <p>contamos con diferentes habitaciones</p>
+                    </article>
+                    <article>
+                        <img src="img/daniel.jpg">
+                        <h4></h4>
+                    </article>
+                    
+                </div>
+            </section>
+            
+            <section id="info">
+                <h3>nuestras habitaciones de acuerdo asu gusto</h3>
+                <div class="contenedor">
+                    <div class="info-pet">
+                        <img src="img/" alt="">
+                        <h4>basico</h4>
+                    </div>
+                    <div class="info-pet">
+                        <img src="img/" alt="">
+                        <h4>confort</h4>
+                    </div>
+                    <div class="info-pet">
+                        <img src="img/images.jpg" alt="">
+                        <h4>tres camas</h4>
+                    </div>
+                    <div class="info-pet">
+                        <img src="img/" alt="">
+                        <h4>switch</h4>
+                    </div>
+                </div>
+            </section>
+        </main>
+        
+        <footer>
+            <div class="contenedor">
+                
+                <div class="sociales">
+                    <a class="fontawesome-facebook-sign" href="#"></a>
+                    <a class="fontawesome-twitter" href="#"></a>
+                    <a class="fontawesome-camera-retro" href="#"></a>
+                    <a class="fontawesome-google-plus-sign" href="#"></a>
+                </div>
+            </div>
+        </footer>
